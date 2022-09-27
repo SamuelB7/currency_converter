93d9daa16831c500fdd643d3501a49bf97843ff5
Samuel Belo <belo.samuel@gmail.com>
2022-09-27 20:46:16 -0300

    autocomplete added, changed to getStaticProps

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,8 @@
-import type { NextPage } from 'next'
+import type { GetStaticProps, NextPage } from 'next'
 import Head from 'next/head'
 import { useState } from 'react'
 import styles from '../styles/Home.module.css'
+import Select from 'react-select'
 
 export function Home({currencies}: any, {convertedCurrency}: any) {
   const [value, setValue] = useState<number>(0)
@@ -21,7 +22,7 @@ export function Home({currencies}: any, {convertedCurrency}: any) {
     let res = await fetch(`https://api.apilayer.com/fixer/convert?to=${to}&from=${from}&amount=${value}`, {
       method: 'GET',
       headers: {
-        apikey : process.env.NEXT_PUBLIC_API_KEY
+        apikey : process.env.NEXT_PUBLIC_API_KEY2
       }
     })
     let data = await res.json()
@@ -30,6 +31,10 @@ export function Home({currencies}: any, {convertedCurrency}: any) {
     setConvertedValue(convertedCurrency)
     return {props: {convertedCurrency}}
   }
+
+  let currencyOptions = currencies.map((currency: any) => (
+    {value: currency[0], label: currency[1]}
+  ))
 
   return (
     <div>
@@ -49,26 +54,28 @@ export function Home({currencies}: any, {convertedCurrency}: any) {
 
           <div className="form-group">
             <label htmlFor="" className="form-label">Convert from:</label>
-            <select className='form-control' name="convertFrom" value={convertFrom} onChange={(e) => {setConvertFrom(e.target.value)}}>
+            <Select options={currencyOptions} placeholder="Select currency" onChange={(e) => setConvertFrom(e.value)}/>
+            {/* <select className='form-control' name="convertFrom" value={convertFrom} onChange={(e) => {setConvertFrom(e.target.value)}}>
               <option value="">Select a currency</option>
               {
                 currencies.map((currency: any) => (
                   <option key={currency[0]} value={currency[0]}>{currency[1]}</option>
                 ))
               }
-            </select>
+            </select> */}
           </div>
 
           <div className="form-group">
             <label htmlFor="" className="form-label">Convert to:</label>
-            <select className='form-control' name="convertTo" value={convertTo} onChange={(e) => {setConvertTo(e.target.value)}}>
+            <Select options={currencyOptions} placeholder="Select currency" onChange={(e) => setConvertTo(e.value)}/>
+            {/* <select className='form-control' name="convertTo" value={convertTo} onChange={(e) => {setConvertTo(e.target.value)}}>
               <option value="">Select a currency</option>
               {
                 currencies.map((currency: any) => (
                   <option key={currency[0]} value={currency[0]}>{currency[1]}</option>
                 ))
               }
-            </select>
+            </select> */}
           </div>
 
           <button onClick={() => convertCurrency(convertFrom, convertTo, value)} type='button' className="btn btn-md btn-primary my-2">Convert</button>
@@ -95,12 +102,24 @@ export function Home({currencies}: any, {convertedCurrency}: any) {
   )
 }
 
-export async function getServerSideProps() {
+/* export async function getServerSideProps() {
   //let res = await fetch(`https://economia.awesomeapi.com.br/json/available/uniq`)
   let res = await fetch(`https://api.apilayer.com/fixer/symbols`, {
     method: 'GET',
     headers: {
-      apikey : process.env.NEXT_PUBLIC_API_KEY
+      apikey : process.env.NEXT_PUBLIC_API_KEY2
+    }
+  })
+  let data = await res.json()
+  let currencies = Object.entries(data.symbols)
+  return { props: { currencies } }
+} */
+
+export const getStaticProps: GetStaticProps = async () => {
+  let res = await fetch(`https://api.apilayer.com/fixer/symbols`, {
+    method: 'GET',
+    headers: {
+      apikey : process.env.NEXT_PUBLIC_API_KEY2
     }
   })
   let data = await res.json()
